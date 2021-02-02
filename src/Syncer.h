6470cba58fdd4f475f13f53c03363f146a0ee533
Balazs Toldi <balazs@toldi.eu>
2021-02-02 23:02:02 +0100

    Added Syncer class

---
+++ src/Syncer.h
@@ -0,0 +1,31 @@
+/* 
+   Copyright (C) 2021 Toldi Balázs Ádám
+  
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  
+
+   File created at: 2021. 02. 02.
+*/
+
+
+
+#ifndef MATRIX_CPP_SDK_SYNCER_H
+#define MATRIX_CPP_SDK_SYNCER_H
+
+
+class Syncer {
+
+};
+
+
+#endif //MATRIX_CPP_SDK_SYNCER_H
